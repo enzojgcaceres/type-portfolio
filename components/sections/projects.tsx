dd890f580fbc6b54b835aa945984dddc9d8bc1e8
enzojgcaceres <enzojgcaceres@gmail.com>
2024-09-26 13:10:10 -0300

    New sites added

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -19,7 +19,7 @@ export function Projects() {
 const images = [
 { image: "/Captura de pantalla (5).png", link: "https://portfolio-three-rouge-20.vercel.app/"},  
 { image: "/travel.png", link: "https://www.youtube.com/watch?v=rREa9L_dQ_M"},
-{ image: "/4vientos-tiendanube.png", link: "https://prefix4vientos2.mitiendanube.com/"},
+{ image: "/4vientos-tiendanube.png", link: "https://sites.google.com/view/somos4vientos/inicio?authuser=0"},
 { image: "/portfolioapp.png", link: "https://portfolio-three-rouge-20.vercel.app/"},
 { image: "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80", link: "https://portfolio-three-rouge-20.vercel.app/"},
 { image: "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80", link: "https://portfolio-three-rouge-20.vercel.app/"},
